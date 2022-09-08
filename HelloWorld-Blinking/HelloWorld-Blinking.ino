2f2f459635b53b006288f5f4fb9c5acf6a581756
tkhamvilai <thanakornkhamvilai@gmail.com>
2022-09-08 16:19:02 -0400

    input pullup

--- HelloWorld-Blinking/HelloWorld-Blinking.ino
+++ HelloWorld-Blinking/HelloWorld-Blinking.ino
@@ -1,10 +1,12 @@
 #define LED_PIN 13
+#define INPUT_PIN 12
 int val;
 
 void setup() {  
   Serial.begin(115200);
   Serial.println("Hello World!");
   pinMode(LED_PIN, OUTPUT);
+  pinMode(INPUT_PIN, INPUT_PULLUP);
 }
 
 void loop() { 
@@ -18,3 +20,21 @@ void loop() {
   Serial.println(val);
   delay(1000);
 }
+
+// void setup() {  
+//   Serial.begin(115200);
+//   Serial.println("Hello World!");
+//   pinMode(LED_PIN, OUTPUT);
+// }
+
+// void loop() { 
+//   digitalWrite(LED_PIN, HIGH);
+//   val = digitalRead(LED_PIN);
+//   Serial.println(val);
+//   delay(1000);
+
+//   digitalWrite(LED_PIN, LOW);
+//   val = digitalRead(LED_PIN);
+//   Serial.println(val);
+//   delay(1000);
+// }
