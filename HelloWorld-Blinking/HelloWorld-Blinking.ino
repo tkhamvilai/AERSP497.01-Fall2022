097d50532e666199ba72c4d1ea22b53cf9135a53
tkhamvilai <thanakornkhamvilai@gmail.com>
2022-09-08 17:52:39 -0400

    proper bit assignments

--- HelloWorld-Blinking/HelloWorld-Blinking.ino
+++ HelloWorld-Blinking/HelloWorld-Blinking.ino
@@ -5,20 +5,39 @@ int val;
 void setup() {  
   Serial.begin(115200);
   Serial.println("Hello World!");
-  DDRB = B11101111;
+  DDRB |= (1 << PB5);
+  DDRB &= ~(1 << PB4);
 }
 
 void loop() { 
-  PORTB = B11111111;
-  val = (PINB >> 5 & B00100000 >> 5);
+  PORTB |= (1 << PB5);
+  val = (PINB & (1 << PB4)) >> 4;
   Serial.println(val);
   delay(1000);
 
-  PORTB = B11011111;
-  val = (PINB >> 5 & B00100000 >> 5);
+  PORTB &= ~(1 << PB5);
+  val = (PINB & (1 << PB4)) >> 4;
   Serial.println(val);
   delay(1000);
 }
+
+// void setup() {  
+//   Serial.begin(115200);
+//   Serial.println("Hello World!");
+//   DDRB = B11101111;
+// }
+
+// void loop() { 
+//   PORTB = B11111111;
+//   val = (PINB >> 5 & B00100000 >> 5);
+//   Serial.println(val);
+//   delay(1000);
+
+//   PORTB = B11011111;
+//   val = (PINB >> 5 & B00100000 >> 5);
+//   Serial.println(val);
+//   delay(1000);
+// }
 
 // void setup() {  
 //   Serial.begin(115200);
@@ -28,12 +47,12 @@ void loop() {
 
 // void loop() { 
 //   *(volatile uint8_t *)0x25 = 0b11111111;
-//   val = ((*(volatile uint8_t *)0x23) >> 5 & 0b00100000 >> 5);
+//   val = ((*(volatile uint8_t *)0x23) >> 4 & 0b00010000 >> 4);
 //   Serial.println(val);
 //   delay(1000);
 
-//   *(volatile uint8_t *)0x25 = B11011111;
-//   val = ((*(volatile uint8_t *)0x23) >> 5 & 0b00100000 >> 5);
+//   *(volatile uint8_t *)0x25 = 0b11011111;
+//   val = ((*(volatile uint8_t *)0x23) >> 4 & 0b00010000 >> 4);
 //   Serial.println(val);
 //   delay(1000);
 // }
@@ -42,6 +61,7 @@ void loop() {
 //   Serial.begin(115200);
 //   Serial.println("Hello World!");
 //   pinMode(LED_PIN, OUTPUT);
+//   pinMode(INPUT_PIN, INPUT_PULLUP);
 // }
 
 // void loop() { 
