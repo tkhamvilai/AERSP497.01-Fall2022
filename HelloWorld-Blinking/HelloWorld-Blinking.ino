c87a7bc6e94c1d60d8f48932e040012235798f6e
tkhamvilai <thanakornkhamvilai@gmail.com>
2022-09-08 16:37:56 -0400

    register named

--- HelloWorld-Blinking/HelloWorld-Blinking.ino
+++ HelloWorld-Blinking/HelloWorld-Blinking.ino
@@ -5,20 +5,38 @@ int val;
 void setup() {  
   Serial.begin(115200);
   Serial.println("Hello World!");
-  *(volatile uint8_t *)0x24 = 0b11101111;
+  DDRB = B11101111;
 }
 
 void loop() { 
-  *(volatile uint8_t *)0x25 = 0b11111111;
-  val = ((*(volatile uint8_t *)0x23) >> 5 & 0b00100000 >> 5);
+  PORTB = B11111111;
+  val = (PINB >> 5 & B00100000 >> 5);
   Serial.println(val);
   delay(1000);
 
-  *(volatile uint8_t *)0x25 = B11011111;
-  val = ((*(volatile uint8_t *)0x23) >> 5 & 0b00100000 >> 5);
+  PORTB = B11011111;
+  val = (PINB >> 5 & B00100000 >> 5);
   Serial.println(val);
   delay(1000);
 }
+
+// void setup() {  
+//   Serial.begin(115200);
+//   Serial.println("Hello World!");
+//   *(volatile uint8_t *)0x24 = 0b11101111;
+// }
+
+// void loop() { 
+//   *(volatile uint8_t *)0x25 = 0b11111111;
+//   val = ((*(volatile uint8_t *)0x23) >> 5 & 0b00100000 >> 5);
+//   Serial.println(val);
+//   delay(1000);
+
+//   *(volatile uint8_t *)0x25 = B11011111;
+//   val = ((*(volatile uint8_t *)0x23) >> 5 & 0b00100000 >> 5);
+//   Serial.println(val);
+//   delay(1000);
+// }
 
 // void setup() {  
 //   Serial.begin(115200);
