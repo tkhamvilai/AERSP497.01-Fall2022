d3a8358d737e61d41476bd7b2b077b8b9e75c1d0
tkhamvilai <thanakornkhamvilai@gmail.com>
2022-09-08 16:34:01 -0400

    direct register control

--- HelloWorld-Blinking/HelloWorld-Blinking.ino
+++ HelloWorld-Blinking/HelloWorld-Blinking.ino
@@ -5,18 +5,17 @@ int val;
 void setup() {  
   Serial.begin(115200);
   Serial.println("Hello World!");
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(INPUT_PIN, INPUT_PULLUP);
+  *(volatile uint8_t *)0x24 = 0b11101111;
 }
 
 void loop() { 
-  digitalWrite(LED_PIN, HIGH);
-  val = digitalRead(LED_PIN);
+  *(volatile uint8_t *)0x25 = 0b11111111;
+  val = ((*(volatile uint8_t *)0x23) >> 5 & 0b00100000 >> 5);
   Serial.println(val);
   delay(1000);
 
-  digitalWrite(LED_PIN, LOW);
-  val = digitalRead(LED_PIN);
+  *(volatile uint8_t *)0x25 = B11011111;
+  val = ((*(volatile uint8_t *)0x23) >> 5 & 0b00100000 >> 5);
   Serial.println(val);
   delay(1000);
 }
