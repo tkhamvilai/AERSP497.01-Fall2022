9795f3ba98c65fdc18b59abef990e809b177cfe5
tkhamvilai <thanakornkhamvilai@gmail.com>
2022-10-10 21:40:47 -0400

    added the sixth channel (AUX2)

--- ReadReceiver/RC.ino
+++ ReadReceiver/RC.ino
@@ -13,13 +13,13 @@
       }                                                \
     }                                                  \
     else edgeTime[chan] = cTime;                       \
-  }                                                    
+  }                                                  
   
 void RX_PIN_MASK(){
+  cli(); //disable other interrupts at this point
   uint8_t mask;
   uint8_t pin;
   uint16_t cTime, dTime;
-  static uint16_t edgeTime[8];
   static uint8_t PCintLast;  
   
   pin = RX_PCINT_PIN_PORT; //read PIN
@@ -29,21 +29,27 @@ void RX_PIN_MASK(){
   PCintLast = pin; //memorize the state of all PIN7:0
    
   //write PPM to each PIN
-  #if (PCINT_PIN_COUNT > 0)
-    RX_PIN_CHECK(0);
-  #endif
-  #if (PCINT_PIN_COUNT > 1)
-    RX_PIN_CHECK(1);
-  #endif
-  #if (PCINT_PIN_COUNT > 2)
-    RX_PIN_CHECK(2);
-  #endif
-  #if (PCINT_PIN_COUNT > 3)
-    RX_PIN_CHECK(3);
-  #endif
-  #if (PCINT_PIN_COUNT > 4)
-    RX_PIN_CHECK(4);
-  #endif
+  RX_PIN_CHECK(ROLL);
+  RX_PIN_CHECK(PITCH);
+  RX_PIN_CHECK(THR);
+  RX_PIN_CHECK(YAW);
+  RX_PIN_CHECK(AUX);
+}
+
+void RX_PIN_MASK2(){
+  cli(); //disable other interrupts at this point
+  uint8_t mask;
+  uint8_t pin;
+  uint16_t cTime, dTime;
+  static uint8_t PCintLast;  
+  
+  pin = RX_PCINT_PIN_PORT2; //read PIN
+  mask = pin ^ PCintLast; //indicate which bit change 
+  cTime = micros(); //keep only 16 bits
+  sei(); //re-enable other interrupts at this point, the rest of this interrupt is not so time critical and can be interrupted safely
+  PCintLast = pin; //memorize the state of all PIN7:0
+
+  RX_PIN_CHECK(AUX2);
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -90,28 +96,38 @@ void mapRCcmd(){
     rcData[chan] = constrain(rcData[chan], MIN_PWM_IN, MAX_PWM_IN);
   }
   
-  RC_Command[ROLL]  = rcData[ROLL]  - ROLL_MID_PWM;
-  RC_Command[PITCH] = rcData[PITCH] - PITCH_MID_PWM;
-  RC_Command[THR]   = rcData[THR];
-  RC_Command[YAW]   = rcData[YAW]   - YAW_MID_PWM;
-  RC_Command[AUX]   = rcData[AUX];
+  RC_Command[ROLL]   = rcData[ROLL]  - ROLL_MID_PWM;
+  RC_Command[PITCH]  = rcData[PITCH] - PITCH_MID_PWM;
+  RC_Command[THR]    = rcData[THR];
+  RC_Command[YAW]    = rcData[YAW]   - YAW_MID_PWM;
+  RC_Command[AUX]    = rcData[AUX];
+  RC_Command[AUX2]   = rcData[AUX2];
 }
 
-//////////////////////////////////////////////////////////////////////////////
-//                                                                          //
-//               PIN mapping : CH1   CH2   CH3   CH4   CH5                  //
-//                             D2    D4    D5    D6    D7                   //
-//                                                                          //
-//////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+//            PIN mapping : CH1   CH2   CH3   CH4   CH5   CH6            //
+//                          D2    D4    D5    D6    D7    D8             //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
 void initPCINT(){  
-  DDRD &= ~(_BV(DDD2) | _BV(DDD4) | _BV(DDD5) | _BV(DDD6) | _BV(DDD7)); //set PORTD as INPUT
+  DDRD &= ~(_BV(DDD2) | _BV(DDD4) | _BV(DDD5) | _BV(DDD6) | _BV(DDD7)); //set PORTD PIN 2,4,5,6,7 as INPUTS
   PORTD |= (_BV(PORTD2) | _BV(PORTD4) | _BV(PORTD5) | _BV(PORTD6) | _BV(PORTD7)); //pull-up HIGH
+
+  DDRB &= ~(_BV(DDB0)); //set PORTB PIN 0 as an INPUT
+  PORTB |= (_BV(PORTB0)); //pull-up HIGH
+
   cli();
-  PCICR |= _BV(PCIE2); // turn on PCMSK2
-  PCMSK2 |= (_BV(PCINT18) | _BV(PCINT20) | _BV(PCINT21) | _BV(PCINT22) | _BV(PCINT23));
+  PCICR  |= (_BV(PCIE2) | _BV(PCIE0)); // turn on PCMSK2 and PCMSK0
+  PCMSK2 |= (_BV(PCINT18) | _BV(PCINT20) | _BV(PCINT21) | _BV(PCINT22) | _BV(PCINT23)); // enable Pin Change Interrupt for PORTD PIN 2,4,5,6,7
+  PCMSK0 |= (_BV(PCINT0)); // enable Pin Change Interrupt for PORTB PIN 0
   sei();
 }
 
 ISR (PCINT2_vect){
   RX_PIN_MASK();
+}
+
+ISR (PCINT0_vect){
+  RX_PIN_MASK2();
 }
--- ReadReceiver/ReadReceiver.ino
+++ ReadReceiver/ReadReceiver.ino
@@ -1,7 +1,8 @@
-#define RC_CHANS 5
-#define PCINT_PIN_COUNT 5
-#define PCINT_RX_BITS (1<<2),(1<<4),(1<<5),(1<<6),(1<<7)
+#define RC_CHANS 6
+#define PCINT_PIN_COUNT 6
+#define PCINT_RX_BITS (1<<2),(1<<4),(1<<5),(1<<6),(1<<7),(1<<0)
 #define RX_PCINT_PIN_PORT PIND
+#define RX_PCINT_PIN_PORT2 PINB
 #define MIN_PWM_IN 1000
 #define MAX_PWM_IN 2000
 #define ROLL_MID_PWM 1500
@@ -12,13 +13,15 @@ static int16_t rcData[RC_CHANS];
 static uint8_t PCInt_RX_Pins[PCINT_PIN_COUNT] = {PCINT_RX_BITS};
 volatile uint16_t rcValue[RC_CHANS];
 volatile int16_t RC_Command[RC_CHANS];
+static uint16_t edgeTime[RC_CHANS];
 
 enum rc {
   ROLL,
   PITCH,
   THR,
   YAW,
-  AUX
+  AUX,
+  AUX2
 };
 
 void setup() {
@@ -36,6 +39,7 @@ void PrintRC(){
   Serial.print(  RC_Command[ROLL]);  Serial.print(", "); 
   Serial.print(  RC_Command[PITCH]); Serial.print(", "); 
   Serial.print(  RC_Command[THR]);   Serial.print(", "); 
-  Serial.print(  RC_Command[YAW]);   Serial.print(", "); 
-  Serial.println(RC_Command[AUX]);
+  Serial.print(  RC_Command[YAW]);   Serial.print(", ");
+  Serial.print(  RC_Command[AUX]);   Serial.print(", "); 
+  Serial.println(RC_Command[AUX2]);
 }
