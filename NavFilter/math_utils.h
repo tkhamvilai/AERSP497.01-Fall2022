39c1ebb101a5990a5ec1b3edf6a32b10c868bdb2
tkhamvilai <thanakornkhamvilai@gmail.com>
2022-11-10 22:20:25 -0500

    added matrix

--- NavFilter/math_utils.h
+++ NavFilter/math_utils.h
@@ -2,6 +2,7 @@
 #define MATH_UTILS_H
 
 #include <BasicLinearAlgebra.h>
+#define Matrix BLA::Matrix
 
 #define DEG2RAD 0.0174533
 #define RAD2DEG 57.2958
@@ -14,12 +15,12 @@
 
 #define GRAVITY 9.81
 
-using state_t = BLA::Matrix<16, 1>;
-using quat_t = BLA::Matrix<4, 1>;
-using vec_t = BLA::Matrix<3, 1>;
-using mat3x3_t = BLA::Matrix<3, 3>;
-using mat4x4_t = BLA::Matrix<4, 4>;
-using euler_t = BLA::Matrix<3, 1>;
+using state_t = Matrix<16, 1>;
+using quat_t = Matrix<4, 1>;
+using vec_t = Matrix<3, 1>;
+using mat3x3_t = Matrix<3, 3>;
+using mat4x4_t = Matrix<4, 4>;
+using euler_t = Matrix<3, 1>;
 
 mat3x3_t rotation_from_quaternion(const quat_t&);
 void normalize_quaternion(quat_t&);
